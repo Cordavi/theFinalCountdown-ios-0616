61a0e514b9f8c53396053ccbd0c96a58c3f32d9d
Cordavi <michaelamundsen@gmail.com>
2016-06-29 17:26:40 -0400

    Refactored to work with pause button functionality allowing you to pause and resume timer

--- theFinalCountdown/FISViewController.m
+++ theFinalCountdown/FISViewController.m
@@ -11,12 +11,14 @@
 @interface FISViewController ()
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
-@property (strong, nonatomic) NSTimer *selectedTimerBasedOnDatePicker;
-@property (strong, nonatomic) NSDate *dateAtStartOfTimer;
-@property (strong, nonatomic) NSDate *dateOfEndOfTimer;
+@property (strong, nonatomic) NSTimer *activeTimer;
+@property (strong, nonatomic) NSDate *startDate;
+@property (strong, nonatomic) NSDate *timerEndDate;
+@property (strong, nonatomic) NSDate *pauseDate;
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
 @property (weak, nonatomic) IBOutlet UIButton *pauseButton;
 @property (weak, nonatomic) IBOutlet UIButton *cancelButton;
+@property (weak, nonatomic) IBOutlet UIButton *resumeButton;
 
 @end
 
@@ -25,12 +27,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
-
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,7 +43,6 @@
 
 
 - (void)resetTimer {
-    [self.selectedTimerBasedOnDatePicker invalidate];
     self.datePicker.hidden = NO;
     self.timeLabel.hidden = YES;
     self.startButton.hidden = NO;
@@ -49,35 +50,59 @@
 }
 
 - (void)updateTimeLabelText {
-    NSTimeInterval timeSinceStartOfTimer = [self.dateAtStartOfTimer timeIntervalSinceNow];
+    NSTimeInterval timeSinceStartOfTimer = [self.timerEndDate timeIntervalSinceNow];
     NSDateComponentsFormatter *countDownText = [[NSDateComponentsFormatter alloc] init];
     self.timeLabel.text = [countDownText stringFromTimeInterval:timeSinceStartOfTimer];
-    if (self.dateAtStartOfTimer.timeIntervalSinceNow <= 0) {
+    if (self.timerEndDate.timeIntervalSinceNow <= 0) {
+        [self.activeTimer invalidate];
         [self resetTimer];
     }
 }
 
+- (void)startTimer:(NSDate *)fireDate {
+    self.timerEndDate = fireDate;
+    NSTimer *countDownTimer = [[NSTimer alloc] init];
+    countDownTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(updateTimeLabelText) userInfo:nil repeats:YES];
+    self.activeTimer = countDownTimer;
+}
+
 - (IBAction)startButtonTapped:(id)sender {
     
-    //stores time picked as an NSDate, creates a timer and set label to selected time
-    self.dateAtStartOfTimer = [NSDate dateWithTimeIntervalSinceNow:self.datePicker.countDownDuration];
-    self.selectedTimerBasedOnDatePicker = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(updateTimeLabelText) userInfo:nil repeats:YES];
-    [self updateTimeLabelText];
+    //start timer
+    [self startTimer:[NSDate dateWithTimeIntervalSinceNow:self.datePicker.countDownDuration]];
+    self.startDate = [NSDate date];
     
     //hides date picker and shows countdown label
     self.datePicker.hidden = YES;
     self.timeLabel.hidden = NO;
     self.startButton.hidden = YES;
     self.cancelButton.hidden = NO;
-    
-}
-
-- (IBAction)pauseButtonTapped:(id)sender {
-    
+    self.pauseButton.enabled = YES;
 }
 
 - (IBAction)cancelButtonTapped:(id)sender {
     [self resetTimer];
+}
+
+
+
+- (IBAction)pauseButtonTapped:(id)sender {
+    [self.activeTimer invalidate];
+    self.pauseDate = [NSDate date];
+    self.pauseButton.hidden = YES;
+    self.pauseButton.enabled = NO;
+    self.resumeButton.hidden = NO;
+    //self.pauseButton.titleLabel.text = @"Resume";
+}
+
+- (IBAction)resumeButtonTapped:(id)sender {
+    NSTimeInterval timeRemaining = [self.startDate timeIntervalSinceDate:self.pauseDate];
+    timeRemaining = self.datePicker.countDownDuration + timeRemaining;
+    NSDate *resumeDate = [NSDate dateWithTimeIntervalSinceNow:timeRemaining];
+    [self startTimer:resumeDate];
+    self.pauseButton.hidden = NO;
+    self.resumeButton.hidden = YES;
+    self.resumeButton.enabled = NO;
 }
 
 @end
